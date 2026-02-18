5384c72ed0f2dd45fc1d943d1782fa1da8edbe17
humeidh <a.humeidh@gmail.com>
2026-02-18 17:24:17 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//websites frontend navoptions, aboutsus, ooms, restaurant,contactus
+Route::get('/about', function () {
+    return view('aboutus');
+}); 
+
+Route::get('/rooms', function () {
+    return view('rooms');
+}); 
+
+Route::get('/restaurant', function () {
+    return view('restaurant');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
